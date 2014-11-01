f6703b61cf7626906bd81b4e9a6095a692f7023b
Shankar, Kumar Shaurya <kumarsha@cs.cmu.edu>
2014-11-01 17:42:47 -0400

    Added skeleton placeholder for motor wrapper

---
+++ nodes/snake_controller.cpp
@@ -0,0 +1,16 @@
+#include <ros/ros.h>
+#include "SnakeMotorWrapper.h"
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "snake_controller");
+  ros::NodeHandle nh;
+
+  SnakeMotorWrapper _wrapper;
+
+  ros::Subscriber sub = nh.subscribe("cmd_vel", 10,&SnakeMotorWrapper::callback, &_wrapper);
+  ros::spin();
+
+  return 0;
+}
---
+++ src/SnakeMotorWrapper.cpp
@@ -0,0 +1,6 @@
+#include "SnakeMotorWrapper.h"
+
+void SnakeMotorWrapper::callback(const geometry_msgs::Twist::ConstPtr& msg)
+{
+
+}
