879730ad1924d04661b287b945ae9776079d1fd0
Kumar Shaurya Shankar <kumarsha@cs.cmu.edu>
2014-11-23 22:49:02 -0500

    Fixed node. Remember to use gscam_nodelet

--- nodes/bag_file_saver.py
+++ nodes/bag_file_saver.py
@@ -12,14 +12,14 @@ from sensor_msgs.msg import Image
 
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
-import rosbag.Bag
+import rosbag
 
 class BagSaver:
     def __init__(self):
-        self.joy_topic = rospy.get_param('~joy_topic', default='\cmd_vel')
-        self.img_topic = rospy.get_param('~img_topic', default='\camera\image_raw')
-        self.execute_topic = rospy.get_param('~execute_topic', default='\execute')
-        self.bag_file_path = rospy.get_param('~bag_file_path')
+        self.joy_topic = rospy.get_param('~joy_topic', default='/cmd_vel')
+        self.img_topic = rospy.get_param('~img_topic', default='/camera/image_raw')
+        self.execute_topic = rospy.get_param('~execute_topic', default='/execute')
+        self.bag_file_path = rospy.get_param('~bag_file_path', default='/home/icoderaven/test.bag')
         self.fix_image = rospy.get_param('~fix_image', default=True)
         
         self.last_joy_msg = None
@@ -32,10 +32,13 @@ class BagSaver:
         self.joy_sub = rospy.Subscriber(self.joy_topic, Twist, self.joy_update, queue_size=1)
         self.img_sub = rospy.Subscriber(self.img_topic, Image, self.img_update, queue_size=1)
         self.empty_sub = rospy.Subscriber(self.execute_topic, Empty, self.execute_update, queue_size=1)  
+	self.publisher = rospy.Publisher('/my_test', Image,queue_size=1); 
     
     def __del__(self):
         self.bag_file.close()
-        
+    
+    def close(self):
+        self.bag_file.close()    
     def joy_update(self, data):
         self.last_joy_msg = data
         
@@ -44,24 +47,26 @@ class BagSaver:
         if self.fix_image == True:
             cv_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
             #Convert from yuv2 to rgb
-            cv_image = cv2.cvtColor(cv_image, cv2.COLOR_YUV2BGR)
-            self.last_img_msg = self.bridge.cv2_to_imgmsg(cv_image, encoding='bgr8')
+            new_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2BGR)
+            self.last_img_msg = self.bridge.cv2_to_imgmsg(new_image, encoding='bgr8')
+            self.publisher.publish(self.last_img_msg)
         else:
             self.last_img_msg = data
         
     def execute_update(self, data):
         '''Write the latest two messages to the bagfile'''
-        if self.last_img_msg is not None and self.last_joy_msg is not None:
-            rospy.loginfo('[BagSaver] Message tuple not received')
-            return False 
-        
-        else:
+        if (self.last_img_msg is not None) and (self.last_joy_msg is not None):
             self.write_bag()
             self.last_img_msg = None
             self.last_joy_msg = None
             return True
+        else:
+            rospy.loginfo('[BagSaver] Message tuple not received')
+            return False 
+            
         
     def write_bag(self):
+	rospy.loginfo('[BagWriter] Adding data tuple')
         self.bag_file.write(self.joy_topic, self.last_joy_msg)
         self.bag_file.write(self.img_topic, self.last_img_msg)
 
@@ -69,3 +74,4 @@ if __name__ == '__main__':
     rospy.init_node('bag_file_saver')
     my_saver = BagSaver()
     rospy.spin()
+    my_saver.close()
