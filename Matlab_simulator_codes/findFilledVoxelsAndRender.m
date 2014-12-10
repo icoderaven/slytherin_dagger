296ae7da4c7bcec3c4eb5b37a3bc239eafb93b67
rarunsrivatsan <rarunsrivatsan@gmail.com>
2014-12-10 18:27:05 -0500

    HAcked the voxelise to get them to where CAD models are

--- Matlab_simulator_codes/findFilledVoxelsAndRender.m
+++ Matlab_simulator_codes/findFilledVoxelsAndRender.m
@@ -1,13 +1,15 @@
-function [ F,fv2] = findFilledVoxelsAndRender(name,R,scale,offset )
-fv2 = stlread(name);
-fv2=scale_stl(fv2,R,scale*2,offset);
-[OUTPUTgrid2] = VOXELISE(20,20,20,name,'xyz');
-[x2,y2,z2]=ind2sub(size(OUTPUTgrid2), find(OUTPUTgrid2));
-voxels2=R*[x2';y2';z2']/scale;
-x2=voxels2(1,:)+offset(1);
-y2=voxels2(2,:)+offset(2);
-z2=voxels2(3,:)+offset(3);
-F=[x2',y2',z2'];
+function [ F,fv] = findFilledVoxelsAndRender(name,R,scale,offset )
+fv = stlread(name);
+fv=scale_stl(fv,R,scale,offset);
+
+%OUTPUTgrid=VOXELISE(20,20,20,fv);
+%[OUTPUTgrid2] = VOXELISE(20,20,20,name,'xyz');
+%[x,y,z]=ind2sub(size(OUTPUTgrid), find(OUTPUTgrid));
+%voxels=R*[x';y';z'];
+%x=voxels(1,:)/scale+offset(1);
+%y=voxels(2,:)/scale+offset(2);
+%z=voxels(3,:)/scale+offset(3);
+F=[0,0,0];
 
 end
 
