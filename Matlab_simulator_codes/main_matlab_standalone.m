fafbe29f0df0002430847617a88e4bbe197c63c4
rarunsrivatsan <rarunsrivatsan@gmail.com>
2014-12-10 16:27:58 -0500

    Updated main_matlab_Standalone. Designed better obstacles

--- Matlab_simulator_codes/main_matlab_standalone.m
+++ Matlab_simulator_codes/main_matlab_standalone.m
@@ -1,7 +1,7 @@
 %%
 close all
 % position of the target
-goal_pt=[40,0,0];
+goal_pt=[180,0,0];
 % Initialize snake variables
 % global_state is where the snake actually advances.
 pitch0=0;
@@ -13,8 +13,8 @@ global_state = [0,0,0,0,0,0,phi0,theta0];
 % state is used for plotting the figures
 state = global_state;
 drawColor=[0.2 length(state)/66 0.3 ];
-LINK_LENGTH=4;
-LINK_RADIUS=1.5;
+LINK_LENGTH=15;
+LINK_RADIUS=15;
 drawType=1;
 Tregister=eye(4);
 linkStartDraw=0;
@@ -22,30 +22,40 @@ linkStartDraw=0;
 
 
 % initialize heart variables
-heart_coord0 = [10,0,0];
-R=[1 0 0; 0 0 -1 ; 0  1 0];
-fv = stlread('heart.STL');
-coords=R*[fv.vertices(:,1)';fv.vertices(:,2)';fv.vertices(:,3)'];
-x=coords(1,:)/10+heart_coord0(1);
-y=coords(2,:)/10+heart_coord0(2);
-z=coords(3,:)/10+heart_coord0(3);
-fv.vertices=[x',y',z'];
 
-% initialize obstacles variables
-obstacles = [goal_pt(1) x;goal_pt(2) y;goal_pt(3) z];
-% plot initial setup
+R=[1 0 0; 0 0 -1 ; 0  1 0];
+scale=1;%everything is defined in cm but stl files are in mm. Hence this scaling.
+
+offset_heart=[30,30,-20];
+[ vox_h,fv] = findFilledVoxelsAndRender('heart.STL',R,scale,offset_heart );
+
+offset_obstacle1=[10,50,0];
+[ vox_obs1,fv1] = findFilledVoxelsAndRender('obstacle.STL',eye(3),scale*1/2,offset_obstacle1 );
+
+offset_obstacle2=[10,-100,0];
+[ vox_obs2,fv2] = findFilledVoxelsAndRender('obstacle.STL',eye(3),scale*1/2,offset_obstacle2 );
+
+%Add more if required
+%offset_obstacle3=[-10,-10,10];
+%[ vox_obs3,fv3] = findFilledVoxelsAndRender('obstacle.STL',R,scale,offset_obstacle3 );
+coords_all=[vox_h;vox_obs1;vox_obs2];
+
+obstacles = [goal_pt(1),coords_all(:,1)' ;goal_pt(2),coords_all(:,2)';goal_pt(3),coords_all(:,3)'];
+
 figure('units','normalized','outerposition',[0 0 1 1])
-axis([0 150 -50 50 -50 50]);
+axis([0 500 -200 200 -200 200]);
 grid on
 hold on
-patch(fv,'FaceColor',[1 0 0],'EdgeColor','none','FaceLighting','gouraud','AmbientStrength', 0.15);
+patch(fv,'FaceColor',[1 0 0],'EdgeColor','none','FaceLighting','gouraud','AmbientStrength', 0.15);%render heart
+patch(fv1,'FaceColor',[1 1 1],'EdgeColor','none','FaceLighting','gouraud','AmbientStrength', 0.15);%render obstacle1
+patch(fv2,'FaceColor',[1 1 0],'EdgeColor','none','FaceLighting','gouraud','AmbientStrength', 0.15);%render obstacle2 . 
 camlight('headlight');
 material('dull');
-hold on
-[~, obstacle] = drawState([10,10,10,0,pi/2,0],[0,0,1],LINK_LENGTH,2*LINK_RADIUS,drawType,Tregister,linkStartDraw);
-scatter3(goal_pt(1),goal_pt(2),goal_pt(3),50,'green','fill')
+
+scatter3(goal_pt(1),goal_pt(2),goal_pt(3),150,'green','fill')
 [h, snakePoints] = drawState(state,drawColor,LINK_LENGTH,LINK_RADIUS,drawType,Tregister,linkStartDraw);
 hold off
+
 %%
 % initialize control variables
 over=0;
@@ -53,7 +63,7 @@ pitch=0;
 yaw=0;
 inc=1*pi/180;
 maxrange=10*pi/180;
-boxsize=10;
+boxsize=100;
 steps=50;
 expert_prob=1;
 while over==0 && length(state)<66
@@ -89,7 +99,7 @@ while over==0 && length(state)<66
     end
     
     % PLOT FOR EXPLORATION
-    plot_currentstate(state,fv,LINK_LENGTH,LINK_RADIUS,Tregister,linkStartDraw,drawType,obstacles,step,maxdist,goal_pt)
+    plot_currentstate(state,fv,fv1,fv2,LINK_LENGTH,LINK_RADIUS,Tregister,linkStartDraw,drawType,obstacles,step,maxdist,goal_pt)
     hold off
     
 end
